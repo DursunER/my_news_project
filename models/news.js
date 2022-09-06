c6ecbc01cd7abc926c4fd05806446579f241ad4d
Dursun Eroglu <dursuneroglu@Dursuns-MacBook-Pro.local>
2022-09-06 11:59:45 +0100

    changed Readme file

---
+++ models/news.js
@@ -0,0 +1,11 @@
+
+const db = require("../db/connection")
+
+exports.getTopics = () => {
+    
+    return db.query(`SELECT * FROM topics `).then((topics) => {
+       
+        return topics.rows
+    })
+
+}
